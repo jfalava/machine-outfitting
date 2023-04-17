7c5d8bcb98ccfc9e884a3522a21cb437b395c6ca
Jorge Fernando Álava <contacto@jfalava.eu>
2023-04-17 03:42:54 +0200

    Initial commit

---
+++ winget-script.ps1
@@ -0,0 +1,79 @@
+### Libraries and Drivers ###
+winget install --id=Microsoft.DotNet.DesktopRuntime.3_1 -e  ;
+winget install --id=Microsoft.DotNet.DesktopRuntime.5 -e  ;
+winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e  ;
+winget install --id=Nvidia.GeForceExperience -e  ;
+### Games and Launchers ###
+winget install --id=Ubisoft.Connect -e  ;
+winget install --id=PerfectWorld.TowerOfFantasy -e  ;
+winget install --id=Valve.Steam -e  ;
+winget install --id=Amazon.Games -e  ;
+winget install --id=EpicGames.EpicGamesLauncher -e  ;
+winget install --id=GOG.Galaxy -e  ;
+winget install --id=ElectronicArts.EADesktop -e  ;
+winget install --id=Blitz.Blitz -e  ;
+winget install --id=goatcorp.XIVLauncher -e  ;
+winget install --id=WowUp.Wowup -e ;
+### Dev Apps ###
+winget install --id=GnuPG.GnuPG -e  ;
+winget install --id=GnuPG.Gpg4win -e  ;
+winget install --id=Python.Python.3.10 -e  ;
+winget install --id=Docker.DockerDesktop -e  ;
+winget install --id=Keybase.Keybase -e  ;
+winget install --id=Microsoft.VisualStudioCode -e  ;
+winget install --id=Git.Git -e  ;
+winget install --id=Github.GithubDesktop -e  ;
+### Sys Apps ###
+winget install --id=Microsoft.PowerShell -e  ;
+winget install --id=Debian.Debian -e  ;
+winget install --id=Canonical.Ubuntu -e  ;
+winget install --id=9P3395VX91NR -e --accept-package-agreements ## msstore Windows Subsystem for Android
+### Media Apps ###
+winget install --id=VideoLAN.VLC -e  ; ## Maybe a slow download 
+winget install --id=TIDALMusicAS.TIDAL -e  ;
+winget install --id=CiderCollective.Cider -e  ;
+### SNS Apps ###
+winget install --id=9NKSQGP7F2NH -e --accept-package-agreements ; ## msstore UWP WhatsApp
+winget install --id=XPDC2RH70K22MN -e --accept-package-agreements ; ## msstore Discord
+winget install --id=Telegram.TelegramDesktop -e  ;
+winget install --id=OpenWhisperSystems.Signal -e  ;
+### Backup Apps ###
+winget install --id=Mega.MEGASync -e  ;
+winget install --id=9NC73MJWHSWW -e --accept-package-agreements ; ## msstore iCloud
+winget install --id=9NC73MJWHSWW -e --accept-package-agreements ; ## msstore SyncFolder
+### WorkFromHome Apps ###
+winget install --id=AnyDeskSoftwareGmbH.AnyDesk -e  ;
+winget install --id=Microsoft.Teams -e  ;
+winget install --id=GlavSoft.TightVNC -e  ;  ## Slow download
+### Tool Apps ###
+winget install --id=M2Team.NanaZip -e  ;
+winget install --id=bozbez.WinCaptureAudio.beta -e  ;
+winget install --id=REALiX.HWiNFO -e  ;
+winget install --id=Nota.Gyazo -e  ;
+winget install --id=Adobe.Acrobat.Reader.64-bit -e  ;
+winget install --id=WinDirStat.WinDirStat -e  ;
+winget install --id=AppWork.JDownloader -e  ;
+winget install --id=OBSProject.OBSStudio -e  ;
+winget install --id=CrystalDewWorld.CrystalDiskInfo -e  ;
+### Windows QoL Apps ###
+winget install --id=ModernFlyouts.ModernFlyouts -e  ;
+winget install --id=Microsoft.PowerToys -e  ;
+winget install --id=9NV4BS3L1H4S -e --accept-package-agreements ; ## msstore QuickLook
+winget install --id=xanderfrangos.twinkletray -e  ;
+### Peripherals Apps ###
+winget install --id=SteelSeries.GG -e  ;
+winget install --id=Logitech.GHUB -e  ;
+### Internet Apps ###
+winget install --id=ProtonTechnologies.ProtonMailBridge -e  ;
+winget install --id=Cisco.CiscoWebexMeetings -e  ;
+winget install --id=VladimirYakovlev.ElectronMail -e  ;
+winget install --id=Brave.Brave -e  ;
+winget install --id=Mozilla.Firefox -e  ;
+winget install --id=qBittorrent.qBittorrent -e  ;
+winget install --id=Bitwarden.Bitwarden -e  ;
+winget install --id=AgileBits.1Password -e  ;
+### VPN and Networking Apps ###
+winget install --id=Cloudflare.cloudflared -e  ;
+winget install --id=NordVPN.NordVPN -e  ;
+winget install --id=ProtonTechnologies.ProtonVPN -e  ;
+winget install --id=Fortinet.FortiClientVPN -e  ;
