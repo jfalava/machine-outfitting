910df682eba42b489b27505fa181bf63990c75db
Jorge Fernando Álava <contacto@jfalava.eu>
2023-09-24 12:40:01 +0200

    Sept2023-2

--- winget-script.ps1
+++ winget-script.ps1
@@ -2,6 +2,8 @@
 winget install --id=Microsoft.DotNet.DesktopRuntime.3_1 -e  ;
 winget install --id=Microsoft.DotNet.DesktopRuntime.5 -e  ;
 winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e  ;
+winget install --id=Microsoft.VCRedist.2012.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2015+.x64 -e  ;
 winget install --id=Nvidia.GeForceExperience -e  ;
 ### Games and Launchers ###
 winget install --id=Ubisoft.Connect -e  ;
@@ -24,8 +26,9 @@ winget install --id=Git.Git -e  ;
 winget install --id=Github.GithubDesktop -e  ;
 ### Sys Apps ###
 winget install --id=Microsoft.PowerShell -e  ;
-winget install --id=9P3395VX91NR -e --accept-package-agreements ## msstore Windows Subsystem for Android
 winget install --id=9P9TQF7MRM4R -e --accept-package-agreements ## msstore Windows Subsystem for Linux
+winget install --id=9P3395VX91NR -e --accept-package-agreements ## msstore Windows Subsystem for Android
+wsl --set-default-version 2
 ### Media Apps ###
 winget install --id=9PFHDD62MXS1 -e --accept-package-agreements ; ## msstore Apple Music Preview
 winget install --id=VideoLAN.VLC -e  ; ## Maybe a slow download 
@@ -61,11 +64,13 @@ winget install --id=Microsoft.PowerToys -e  ;
 winget install --id=xanderfrangos.twinkletray -e  ;
 ### Peripherals Apps ###
 winget install --id=SteelSeries.GG -e  ;
+winget install --id=Logitech.OptionsPlus -e  ;
 winget install --id=Logitech.GHUB -e  ;
 ### Internet Apps ###
 winget install --id=ProtonTechnologies.ProtonMailBridge -e  ;
+winget install --id=Proton.ProtonDrive -e  ;
+winget install --id=Readdle.Spark -e  ;
 winget install --id=Cisco.CiscoWebexMeetings -e  ;
-winget install --id=VladimirYakovlev.ElectronMail -e  ;
 winget install --id=Brave.Brave -e  ;
 winget install --id=LibreWolf.LibreWolf -e  ;
 winget install --id=qBittorrent.qBittorrent -e  ;
