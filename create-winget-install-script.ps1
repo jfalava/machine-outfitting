913bcbb8f050edaf05e6c9f06e304da905844bcc
Jorge Fernando Álava <github@mail.jfalava.eu>
2024-01-23 22:14:12 +0100

    Updated with a less verbose create script

--- create-winget-install-script.ps1
+++ create-winget-install-script.ps1
@@ -1,8 +1,6 @@
 # Log the start of the script execution
-Write-Host "Starting to generate the winget install script..."
 
 # Get the list of installed packages via winget
-Write-Host "Retrieving installed packages from winget..."
 $installedPackagesOutput = winget list
 
 # Check if output is empty
@@ -12,10 +10,6 @@ if (-not $installedPackagesOutput) {
 } else {
     Write-Host "Successfully retrieved the list of installed packages."
 }
-
-# Output the 'winget list' result for debugging
-Write-Host "Output from 'winget list':"
-$installedPackagesOutput | Write-Host
 
 # Parse the output into an array, skipping the first two lines (headers)
 $installedPackages = $installedPackagesOutput -split "`r`n" | Select-Object -Skip 2
@@ -39,8 +33,6 @@ foreach ($package in $installedPackages) {
                 if ($packageId -match $validPackageIdPattern) {
                     $installCommand = "winget install $packageId --accept-package-agreements --accept-source-agreements"
                     $installScriptContent += $installCommand
-                    # Echo the package Id to the console
-                    Write-Host "Parsed package: ID = $packageId, Origen = $origen"
                 } else {
                     Write-Host "Skipping invalid package: ID = $packageId, Origen = $origen"
                 }
@@ -54,7 +46,6 @@ $installScriptPath = "winget-install-script.ps1"
 
 # Check if there are any install commands to write to the script
 if ($installScriptContent) {
-    Write-Host "Writing install commands to script..."
     $installScriptContent | Out-File -FilePath $installScriptPath -Encoding UTF8
     Write-Host "Winget install script has been created at: $installScriptPath"
 } else {
