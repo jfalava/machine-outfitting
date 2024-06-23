3cd8505093ac85bc1fe3ef7a98c5249346c99fa4
Jorge Fernando Álava <git@jfa.dev>
2024-06-23 13:35:25 +0200

    summer package update

--- winget-install-script.ps1
+++ winget-install-script.ps1
@@ -14,25 +14,26 @@ winget install --id=Microsoft.EdgeWebView2Runtime -e  ;
 ###########################
 winget install --id=Ubisoft.Connect -e  ;
 winget install --id=Valve.Steam -e  ;
-winget install --id=Amazon.Games -e  ; # Sometimes returns bad hash
+winget install --id=Amazon.Games -e  ; # Sometimes returns bad hash, careful
 winget install --id=EpicGames.EpicGamesLauncher -e  ;
 winget install --id=GOG.Galaxy -e  ;
-winget install --id=ElectronicArts.EADesktop -e  ; # Sometimes returns bad hash
-winget install --id=Blitz.Blitz -e  ;
+winget install --id=ElectronicArts.EADesktop -e  ; # Sometimes returns bad hash, careful
 winget install --id=goatcorp.XIVLauncher -e  ;
+winget install --id=WeakAuras.WeakAurasCompanion -e ;
 winget install --id=WowUp.Wowup -e ;
 winget install --id=NexusMods.Vortex -e ;
 ################
 ### Dev Apps ###
 ################
-# IDE, LSPs, Compile Engines and Version Control Packages
-winget install --id=Chocolatey.Chocolatey -e  ;
 winget install --id=Microsoft.VisualStudioCode -e  ;
-winget install --id=Microsoft.VisualStudio.2022.BuildTools -e  ; ## Just works with MS editors, bunch of baboons
+winget install --id=Microsoft.VisualStudio.2022.BuildTools -e  ;
 winget install --id=Kitware.CMake -e  ;
 winget install --id=JetBrains.Toolbox -e  ;
 winget install --id=JanDeDobbeleer.OhMyPosh -s winget -e  ;
 winget install --id=Microsoft.Git -e  ;
+winget install --id=Neovim.Neovim -e ;
+git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
+Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
 ## PGP
 winget install --id=GnuPG.GnuPG -e  ;
 winget install --id=GnuPG.Gpg4win -e  ;
@@ -61,7 +62,6 @@ winget install --id=ChatterinoTeam.Chatterino -e  ;
 ################
 winget install --id=9NKSQGP7F2NH -e --accept-package-agreements ; ## msstore UWP WhatsApp
 winget install --id=XPDC2RH70K22MN -e --accept-package-agreements ; ## msstore Discord
-winget install --id=Discord.Discord.Canary -e  ;
 winget install --id=Telegram.TelegramDesktop -e  ;
 winget install --id=OpenWhisperSystems.Signal -e  ;
 ###################
@@ -70,11 +70,12 @@ winget install --id=OpenWhisperSystems.Signal -e  ;
 winget install --id=Mega.MEGASync -e  ; # Sometimes returns bad hash
 winget install --id=9PKTQ5699M62 -e --accept-package-agreements ; ## msstore iCloud
 winget install --id=9NC73MJWHSWW -e --accept-package-agreements ; ## msstore SyncFolder
+winget install --id=HaystackSoftwareLLC.Arq7 -e ;
 #########################
 ### WorkFromHome Apps ###
 #########################
-winget install --id=AnyDeskSoftwareGmbH.AnyDesk -e  ;
 winget install --id=Microsoft.Teams -e  ;
+winget install --id=SlackTechnologies.Slack -e ;
 #################
 ### Tool Apps ###
 #################
@@ -96,14 +97,21 @@ winget install --id=Bin-Huang.Chatbox -e  ;
 winget install --id=ModernFlyouts.ModernFlyouts -e  ;
 winget install --id=Microsoft.PowerToys -e  ;
 winget install --id=xanderfrangos.twinkletray -e  ;
+#################
+### CLI Tools ###
+#################
 winget install --id=BurntSushi.ripgrep.MSVC -e  ;
 winget install --id=sharkdp.bat -e  ;
 winget install --id=ajeetdsouza.zoxide -e ;
 winget install --id=eza-community.eza -e ;
-########################
-### Peripherals Apps ###
-########################
+winget install --id=gsass1.NTop -e ;
+winget install --id=Chocolatey.Chocolatey -e  ;
+winget install --id=nepnep.neofetch-win -e ;
+##################################
+### Devices Companion Apps ###
+##################################
 winget install --id=SteelSeries.GG -e  ;
+winget install --id=9PFHDD62MXS1 -e --accept-package-agreements ; ## msstore Apple Devices
 winget install --id=Logitech.OptionsPlus -e  ;
 winget install --id=Logitech.GHUB -e  ;
 #####################
@@ -115,11 +123,11 @@ winget install --id=Mozilla.Firefox -e  ;
 winget install --id=TheBrowserCompany.Arc -e  ;
 winget install --id=AgileBits.1Password -e  ;
 winget install --id=AgileBits.1Password.CLI -e  ;
-winget install --id=VladimirYakovlev.ElectronMail -e  ;
 winget install --id=Proton.ProtonMail -e  ;
 winget install --id=Proton.ProtonMailBridge -e  ;
 winget install --id=Proton.ProtonPass -e  ;
 winget install --id=Proton.ProtonDrive -e  ;
+winget install --id=Mozilla.Thunderbird -e ;
 ###############################
 ### VPN and Networking Apps ###
 ###############################
@@ -134,6 +142,5 @@ choco install ninja
 ############################
 ### Extra Setup Commands ###
 ############################
-wsl --set-default-version 2
 Install-Module posh-git
 # Get-PoshThemes for more PS themes
