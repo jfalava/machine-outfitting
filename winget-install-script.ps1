3552e992a8830a71f8bef7fdad30fada9898a45a
Jorge Fernando Álava <git@jfa.dev>
2024-09-01 22:00:54 +0200

    Gaming rig focused changes

--- winget-install-script.ps1
+++ winget-install-script.ps1
@@ -4,10 +4,16 @@
 winget install --id=Microsoft.DotNet.DesktopRuntime.3_1 -e  ;
 winget install --id=Microsoft.DotNet.DesktopRuntime.5 -e  ;
 winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e  ;
-winget install --id=Microsoft.VCRedist.2012.x64 -e  ;
-winget install --id=Microsoft.VCRedist.2015+.x64 -e  ;
-winget install --id=Microsoft.VCRedist.2008.x64 -e  ;
 winget install --id=Microsoft.VCRedist.2005.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2008.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2010.x86 -e  ;
+winget install --id=Microsoft.VCRedist.2012.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2012.x86 -e  ;
+winget install --id=Microsoft.VCRedist.2013.x86 -e  ;
+winget install --id=Microsoft.VCRedist.2013.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2015+.x64 -e  ;
+winget install --id=Microsoft.VCRedist.2015+.x86 -e  ;
+winget install --id=Microsoft.VCLibs.Desktop.14 -e  ;
 winget install --id=Microsoft.EdgeWebView2Runtime -e  ;
 ###########################
 ### Games and Launchers ###
@@ -26,7 +32,7 @@ winget install --id=NexusMods.Vortex -e ;
 ### Dev Apps ###
 ################
 winget install --id=Microsoft.VisualStudioCode -e  ;
-dwinget install --id=JanDeDobbeleer.OhMyPosh -s winget -e  ;
+winget install --id=JanDeDobbeleer.OhMyPosh -s winget -e  ;
 winget install --id=Microsoft.Git -e  ;
 #################
 ### CLI Tools ###
@@ -36,7 +42,7 @@ winget install --id=sharkdp.bat -e  ;
 winget install --id=ajeetdsouza.zoxide -e ;
 winget install --id=eza-community.eza -e ;
 winget install --id=gsass1.NTop -e ;
-winget install --id=nepnep.neofetch-win -e ;
+winget install --id=Fastfetch-cli.Fastfetch -e ;
 ################
 ### Sys Apps ###
 ################
@@ -56,13 +62,10 @@ winget install --id=ChatterinoTeam.Chatterino -e  ;
 winget install --id=9NKSQGP7F2NH -e --accept-package-agreements ; ## msstore UWP WhatsApp
 winget install --id=XPDC2RH70K22MN -e --accept-package-agreements ; ## msstore Discord
 winget install --id=Telegram.TelegramDesktop -e  ;
-winget install --id=OpenWhisperSystems.Signal -e  ;
 ###################
 ### Backup Apps ###
 ###################
 winget install --id=Mega.MEGASync -e  ; # Sometimes returns bad hash
-winget install --id=9PKTQ5699M62 -e --accept-package-agreements ; ## msstore iCloud
-winget install --id=9NC73MJWHSWW -e --accept-package-agreements ; ## msstore SyncFolder
 winget install --id=HaystackSoftwareLLC.Arq7 -e ;
 #################
 ### Tool Apps ###
@@ -86,22 +89,20 @@ winget install --id=KDE.Krita -e  ;
 winget install --id=ModernFlyouts.ModernFlyouts -e  ;
 winget install --id=Microsoft.PowerToys -e  ;
 winget install --id=xanderfrangos.twinkletray -e  ;
-##################################
+##############################
 ### Devices Companion Apps ###
-##################################
-winget install --id=SteelSeries.GG -e  ;
+##############################
 winget install --id=9PFHDD62MXS1 -e --accept-package-agreements ; ## msstore Apple Devices
+winget install --id=SteelSeries.GG -e  ;
 winget install --id=Logitech.OptionsPlus -e  ;
 winget install --id=Logitech.GHUB -e  ;
 #####################
 ### Internet Apps ###
 #####################
 winget install --id=qBittorrent.qBittorrent -e  ;
-winget install --id=Brave.Brave -e  ;
 winget install --id=Mozilla.Firefox -e  ;
 winget install --id=TheBrowserCompany.Arc -e  ;
 winget install --id=AgileBits.1Password -e  ;
-winget install --id=ZenBrowser.Zen -e;
 ###############################
 ### VPN and Networking Apps ###
 ###############################
